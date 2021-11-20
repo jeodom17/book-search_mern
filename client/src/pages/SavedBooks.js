b4f1577ca615d174f4753256344126a19384d607
jeodom17 <jeodom17@gmail.com>
2021-11-19 19:49:35 -0500

    fix errors in SavedBooks.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 // import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
@@ -13,7 +13,7 @@ const SavedBooks = () => {
   });
   const userData = data?.me;
   
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const removeBook = useMutation(REMOVE_BOOK);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
